e8d57d0fdb5d3b6353f11a1ddbd85dbc56f01910
Maxwell <philomath241199@gmail.com>
2022-11-24 16:36:15 +0800

    Add global exception handling

--- src/main/java/com/ministore/pointofsale/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ministore/pointofsale/exception/GlobalExceptionHandler.java
@@ -2,15 +2,17 @@ package com.ministore.pointofsale.exception;
 
 import com.ministore.pointofsale.vo.ResponseVO;
 import com.ministore.pointofsale.vo.ServiceStatusCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler
-    public ResponseVO<ServiceStatusCode> handleServiceException(ServiceException ex) {
-        return new ResponseVO<>(new ServiceStatusCode(ex.getCode(), ex.getMessage()));
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<ServiceStatusCode> handleServiceException(ServiceException ex) {
+        return new ResponseEntity<>(new ServiceStatusCode(ex.getCode(), ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
 }
